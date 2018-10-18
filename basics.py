aa70ef085d58ccb934638b173e7e8795659ef921
Shiv Krishna Dayal <shivshankar.dayal@gmail.com>
2018-10-18 23:00:36 +0530

    basics of python3

---
+++ basics.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import math
+
+print('Hello world\n')
+print("Hello world")
+
+x = 2 + 2
+print(x)
+
+x = input('Enter an integer:')
+
+print(type(x))
+x = int(x)
+print(int(x), type(x))
+
+print('You entered ' + str(x))
+
+print(math.pi)
+
+print(math.cos(math.pi))
+
+x = 7863458636568356235863 * 873645768386465873658364
+
+print(x)
