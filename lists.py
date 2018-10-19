b6dc01090ef50136330f136ee8ae6e223415c406
Shiv Krishna Dayal <shivshankar.dayal@gmail.com>
2018-10-19 19:08:59 +0530

    control flow programs

---
+++ lists.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# python has several useful data structures and it is called a language with batteries included
+
+# Most useful are strings, lists, dictionaries, tuples and sets.
+
+# lists, dictionaries and sets are mutable(meaning they can change) while strings and tuples are immutable(i.e. they do
+# not change)
+
+l = []
+
